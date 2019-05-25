865090cadf84295dce7843e977974ad57e0dabb9
Matthias Yang He <myh7@psu.edu>
2019-05-25 18:48:49 -0400

    Temporary script

---
+++ examples/generate_catalogs_emulator.jl
@@ -0,0 +1,6 @@
+
+include(joinpath(dirpath, "../src/clusters.jl"))
+
+##### To load model parameters found using the GP emulator and simulate catalogs if they pass a distance threshold:
+
+#GP_points = CSV.read()
