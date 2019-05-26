2c11fe60a359736a7e98b58d02a3770f4840ff95
Matthias Yang He <myh7@psu.edu>
2019-05-26 10:42:52 -0400

    Delete generate_catalogs_emulator.jl

--- examples/generate_catalogs_emulator.jl
+++
@@ -1,6 +0,0 @@
-
-include(joinpath(dirpath, "../src/clusters.jl"))
-
-##### To load model parameters found using the GP emulator and simulate catalogs if they pass a distance threshold:
-
-#GP_points = CSV.read()
