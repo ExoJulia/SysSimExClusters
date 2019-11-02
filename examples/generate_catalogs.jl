4c8a416a1bd375d71a34f58759d0a5af08023e65
Matthias Yang He <myh7@psu.edu>
2019-11-02 18:48:25 -0400

    Draw, print, and use a random seed for generating catalogs for reproducibility/debugging

--- examples/generate_catalogs.jl
+++ examples/generate_catalogs.jl
@@ -5,7 +5,12 @@ include(joinpath(dir_path, "../src/clusters.jl"))
 ##### To generate one physical and observed catalog:
 
 sim_param = setup_sim_param_model()
-add_param_fixed(sim_param,"num_targets_sim_pass_one", 79935)
+add_param_fixed(sim_param,"num_targets_sim_pass_one", 79935*5)
+
+using Random
+rng_seed = rand(1:10000)
+println("Random seed: ", rng_seed)
+Random.seed!(rng_seed)
 
 cat_phys, cat_phys_cut, cat_obs, summary_stat = generate_and_save_physical_and_observed_catalogs(sim_param)
 println("Finished generating physical and observed catalogs.")
