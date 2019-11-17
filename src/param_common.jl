785333f34c917ed764f3d8c08a6555146732ca41
Matthias Yang He <myh7@psu.edu>
2019-11-17 14:41:28 -0500

    Add params for fswp and bprp dependence and new split stars weights file

--- src/param_common.jl
+++ src/param_common.jl
@@ -29,15 +29,15 @@ function setup_sim_param_model(args::Vector{String} = Array{String}(undef, 0)) #
     # For generating planetary system properties:
     add_param_fixed(sim_param,"generate_planetary_system", generate_planetary_system_clustered) # For Non-clustered model: "generate_planetary_system_non_clustered"
 
-    add_param_active(sim_param,"f_stars_with_planets_attempted_color_slope", 0.)
-    add_param_active(sim_param,"f_stars_with_planets_attempted_at_med_color", 0.5)
-    add_param_fixed(sim_param,"med_color", 0.95)
-    #add_param_active(sim_param,"f_stars_with_planets_attempted", 1.)
+    #add_param_active(sim_param,"f_stars_with_planets_attempted_color_slope", 0.)
+    #add_param_active(sim_param,"f_stars_with_planets_attempted_at_med_color", 0.5)
+    #add_param_fixed(sim_param,"med_color", 0.95)
+    add_param_active(sim_param,"f_stars_with_planets_attempted", 0.5)
     add_param_fixed(sim_param,"generate_num_clusters", generate_num_clusters_ZTP)
     add_param_fixed(sim_param,"generate_num_planets_in_cluster", generate_num_planets_in_cluster_ZTP)
-    add_param_active(sim_param,"log_rate_clusters", log(1.6))
+    add_param_active(sim_param,"log_rate_clusters", log(0.8))
     add_param_fixed(sim_param,"max_clusters_in_sys", 10)
-    add_param_active(sim_param,"log_rate_planets_per_cluster", log(1.6))
+    add_param_active(sim_param,"log_rate_planets_per_cluster", log(3.7))
     add_param_fixed(sim_param,"max_planets_in_cluster", 10)
 
     # Generate_num_planets_in_cluster currently calls:
@@ -45,8 +45,8 @@ function setup_sim_param_model(args::Vector{String} = Array{String}(undef, 0)) #
     add_param_fixed(sim_param,"generate_sizes", ExoplanetsSysSim.generate_sizes_broken_power_law) # To choose the way we draw planetary radii; if "generate_sizes_power_law", then takes "power_law_r"; if "generate_sizes_broken_power_law", then takes "power_law_r1", "power_law_r2", and "break_radius"
     add_param_active(sim_param,"power_law_P", 0.)
     #add_param_fixed(sim_param,"power_law_r", -2.5)
-    add_param_active(sim_param,"power_law_r1", -1.)
-    add_param_active(sim_param,"power_law_r2", -4.)
+    add_param_active(sim_param,"power_law_r1", -1.0)
+    add_param_active(sim_param,"power_law_r2", -4.4)
     add_param_fixed(sim_param,"min_period", 3.0)
     add_param_fixed(sim_param,"max_period", 300.0)
     add_param_fixed(sim_param,"min_radius", 0.5*ExoplanetsSysSim.earth_radius)
@@ -56,23 +56,23 @@ function setup_sim_param_model(args::Vector{String} = Array{String}(undef, 0)) #
     # Generate_num_planets_in_cluster currently use these for the inclination distribution:
     add_param_fixed(sim_param,"resonance_width", 0.05)
     add_param_fixed(sim_param,"period_ratios_mmr", [2.0, 1.5, 4/3, 5/4])
-    add_param_active(sim_param,"f_high_incl", 0.5) # fraction of systems with higher mutual inclinations
-    add_param_active(sim_param,"sigma_incl", 10.) # degrees; 0 = coplanar w/ generate_kepler_target_simple; ignored by generate_planetary_system_uncorrelated_incl
-    add_param_active(sim_param,"sigma_incl_near_mmr", 1.)
+    add_param_active(sim_param,"f_high_incl", 0.4) # fraction of systems with higher mutual inclinations
+    add_param_active(sim_param,"sigma_incl", 50.) # degrees; 0 = coplanar w/ generate_kepler_target_simple; ignored by generate_planetary_system_uncorrelated_incl
+    add_param_active(sim_param,"sigma_incl_near_mmr", 1.4)
 
     add_param_fixed(sim_param,"max_incl_sys", 0.0) #degrees; gives system inclinations from "max_incl_sys" (deg) to 90 (deg), so set to 0 for isotropic distribution of system inclinations; NOTE: make sure the difference between this and 90 (deg) is at least greater than "sigma_incl" and "sigma_incl_near_mmr"!
 
     # Generate_num_planets_in_cluster currently use these for the eccentricity distribution:
     add_param_fixed(sim_param,"generate_e_omega", ExoplanetsSysSim.generate_e_omega_rayleigh)
-    add_param_active(sim_param,"sigma_hk", 0.01)
+    add_param_active(sim_param,"sigma_hk", 0.02)
     #add_param_fixed(sim_param,"sigma_hk_one", 0.1)
     #add_param_fixed(sim_param,"sigma_hk_multi", 0.03)
 
     # Generate_num_planets_in_cluster currently use these for the stability tests:
     add_param_fixed(sim_param,"num_mutual_hill_radii", 8.0)
     add_param_fixed(sim_param,"generate_planet_mass_from_radius", generate_planet_mass_from_radius_Ning2018_table) # "ExoplanetsSysSim.generate_planet_mass_from_radius_powerlaw" or "generate_planet_mass_from_radius_Ning2018" or "generate_planet_mass_from_radius_Ning2018_table"
-    add_param_active(sim_param,"sigma_log_radius_in_cluster", 0.25)
-    add_param_active(sim_param,"sigma_logperiod_per_pl_in_cluster", 0.15)
+    add_param_active(sim_param,"sigma_log_radius_in_cluster", 0.3)
+    add_param_active(sim_param,"sigma_logperiod_per_pl_in_cluster", 0.2)
 
     # Functions to calculate observables from physical system properties:
     add_param_fixed(sim_param,"calc_target_obs_single_obs", ExoplanetsSysSim.calc_target_obs_single_obs)
@@ -105,6 +105,8 @@ function write_model_params(f, sim_param::SimParam)
     println(f, "# num_targets_sim_pass_one: ", get_int(sim_param,"num_targets_sim_pass_one"))
     println(f, "# max_incl_sys: ", get_real(sim_param,"max_incl_sys"))
     println(f, "# f_stars_with_planets_attempted: ", get_real(sim_param,"f_stars_with_planets_attempted"))
+    #println(f, "# f_stars_with_planets_attempted_at_med_color: ", get_real(sim_param,"f_stars_with_planets_attempted_at_med_color"))
+    #println(f, "# f_stars_with_planets_attempted_color_slope: ", get_real(sim_param,"f_stars_with_planets_attempted_color_slope"))
     println(f, "# generate_num_clusters: ", string(get_function(sim_param,"generate_num_clusters")))
     println(f, "# log_rate_clusters: ", get_real(sim_param,"log_rate_clusters"))
     println(f, "# max_clusters_in_sys: ", get_int(sim_param,"max_clusters_in_sys"))
